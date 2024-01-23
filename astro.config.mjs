18b23592141a60c21a9fff99b349901eb630a7b4
omailhot <olivier.mailhot2@gmail.com>
2024-01-23 17:56:35 -0500

    Add new images, configuration files, and components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,21 @@
 import { defineConfig } from 'astro/config';
-
 import cloudflare from "@astrojs/cloudflare";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [tailwind()],
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "fr"],
+    fallback: {
+      fr: "en"
+    },
+    routing: {
+      prefixDefaultLocale: true
+    }
+  }
 });
